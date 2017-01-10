f72608f5cfec50d3435fdb77c12ff1fd9fa3dd5d
sternenseemann <git@lukasepple.de>
2017-01-10 18:14:44 +0100

    well…

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,6 +11,5 @@ set -x GOPATH /home/lukas/src/go
 
 alias wow='git status'
 alias ll="ls -lha"
-alias uberspace="mosh -p 64374 lukase@lukasepple.de"
 alias tma='tmux attach -d -t'
 alias sd='systemctl'
