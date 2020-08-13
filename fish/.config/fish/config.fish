e613a78a0e04e8f657256ac6d19382390a47b74c
sternenseemann <sternenseemann@systemli.org>
2020-08-13 17:32:31 +0200

    fish: no GOPATH, no greeting, firefox, sbcl wrapper

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,17 @@
-set -x BROWSER chromium
+set -x BROWSER firefox
 set -x PAGER "less -R"
 set -x EDITOR nvim
 set -x DOTREMINDERS $HOME/files/orga/reminders
 
-set -e fish_greeting
+set -x fish_greeting ""
 
 set -x GPG_TTY (tty)
-
-set -x GOPATH /home/lukas/src/go
 
 alias wow='git status'
 alias ll="ls -lha"
 alias tma='tmux attach -d -t'
 alias sd='systemctl'
 alias prefetch-sha256='nix-prefetch-url --type sha256'
+alias rsbcl='rlwrap sbcl'
 
 set -x PATH "$PATH:$HOME/bin"
